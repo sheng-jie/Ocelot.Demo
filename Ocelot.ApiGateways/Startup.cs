fc996744e3449c3d962ae5c9b144dbfc3116fd69
shengjie_yan <ysjshengjie@live.cn>
2019-02-27 08:45:22 +0800

    Add Ocelot Nuget

--- Ocelot.ApiGateways/Startup.cs
+++ Ocelot.ApiGateways/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace Ocelot.ApiGateways
 {
@@ -26,6 +28,8 @@ namespace Ocelot.ApiGateways
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddOcelot(new ConfigurationBuilder().AddJsonFile("ocelot.json").Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +46,8 @@ namespace Ocelot.ApiGateways
             }
 
             app.UseHttpsRedirection();
+
+            app.UseOcelot().Wait();
             app.UseMvc();
         }
     }
