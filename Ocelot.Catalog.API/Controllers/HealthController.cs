e6b1f52b3506e39873282f5bfec06949f61b9d30
shengjie_yan <ysjshengjie@live.cn>
2019-02-27 16:55:46 +0800

    add health check

---
+++ Ocelot.Catalog.API/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ocelot.Catalog.API.Controllers
+{
+    [Route("api/[controller]"), ApiController]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// Health check
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
