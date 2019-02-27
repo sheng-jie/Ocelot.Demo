8c18db0f6aebade9f2fe937f14901a7c155eda9f
shengjie_yan <ysjshengjie@live.cn>
2019-02-27 14:59:35 +0800

    add authentication middleware

--- Ocelot.Basket.API/Controllers/BasketController.cs
+++ Ocelot.Basket.API/Controllers/BasketController.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ocelot.Basket.API.Controllers {
     [Route ("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BasketController : ControllerBase {
         private static List<CustomerBasket> _customerBaskets;
 
--- Ocelot.Basket.API/Startup.cs
+++ Ocelot.Basket.API/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Ocelot.Basket.API
 {
@@ -26,6 +19,13 @@ namespace Ocelot.Basket.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", option =>
+                {
+                    option.Authority = "http://localhost:6003";
+                    option.RequireHttpsMetadata = false;
+                    option.Audience = "basket.api";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +42,7 @@ namespace Ocelot.Basket.API
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
