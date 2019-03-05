45957cbf783a5f90b29dadc34cfdb2a4bf9213ea
shengjie <ysjshengjie@live.cn>
2019-03-05 21:24:12 +0800

    Use HealthCheck middleware.

--- Ocelot.Basket.API/Startup.cs
+++ Ocelot.Basket.API/Startup.cs
@@ -19,6 +19,7 @@ namespace Ocelot.Basket.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHealthChecks();
             services.AddAuthentication("Bearer")
                 .AddJwtBearer("Bearer", option =>
                 {
@@ -40,7 +41,7 @@ namespace Ocelot.Basket.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseHealthChecks("/hc");
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
